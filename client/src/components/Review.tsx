9c33c1b534d4f22808e59ad9ac412f5a355a4a65
ullltimate <ullltimate.zp@gmail.com>
2023-09-08 12:00:23 +0300

    feat: add rating functional in review page

--- client/src/components/Review.tsx
+++ client/src/components/Review.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import Header from './Header';
 import { Col, Container, Row, Image } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
@@ -6,6 +6,8 @@ import { useTranslation } from "react-i18next";
 import { useTheme } from '../hooks/useTheme';
 import { getReview } from '../api/reviews';
 import { IReview } from '../types/types';
+import ReactStars from 'react-rating-star-with-type';
+import { addRating, getRating } from '../api/rating';
 
 function Review() {
     const params = useParams();
@@ -14,10 +16,29 @@ function Review() {
     const { t, i18n: {changeLanguage, language} } = useTranslation();
 	const [currentLanguage, setCurrentLanguage] = useState(language);
     const {theme, setTheme} = useTheme();
+    const user = localStorage.getItem('user');
+    const [star, setStar] = useState(0);
+    const [editStar, setEditStar] = useState(true);
+    const [rating, setRating] = useState(0);
+    const navigate = useNavigate();
 
     useEffect(()=>{
         if (idReview) getReview(idReview, setReview);
     },[])
+
+    useEffect(()=>{
+        (user && idReview) ? getRating(idReview, setRating, JSON.parse(user)._id, setStar, setEditStar,) : (idReview) && getRating(idReview, setRating);
+    },[star])
+    
+    async function rate(nextValue: any){
+        if(user && idReview){
+            await addRating(idReview, JSON.parse(user)._id, nextValue);
+            setStar(nextValue);
+            setEditStar(false);
+        } else {
+            navigate('/login');
+        }
+    }
 
   	return (
   	  <>
@@ -31,10 +52,27 @@ function Review() {
                         <Image src={review.img} style={{width: '100%', height: '100%', objectFit: 'contain'}} />
                     </Col>
                     <Col>
-                        <h3>{review.title}</h3>
-                        <p>{t('review.group')} {review.group}</p>
-                        <p>{t('review.score')} {review.score}/10</p>
-                        <p>{t('review.tags')} #{review.tags.map((e:any) => e).join('#')}</p>
+                        <Row>
+                            <Col>
+                                <h3>{review.title}</h3><p>{t('review.group')} {review.group}</p>
+                                <p>{t('review.score')} {review.score}/10</p>
+                                <p>{t('review.tags')} #{review.tags.map((e:any) => e).join('#')}</p>
+                                <p>{t('review.posted')} {Intl.DateTimeFormat(currentLanguage).format(review.creationDate)}</p>
+                            </Col>
+                            <Col className='text-end'>
+                                <span>{rating}</span>
+                                <i className="bi bi-star-fill text-warning"></i>
+                                <ReactStars 
+                                    key={`stars_${star}`}
+                                    onChange={rate} 
+                                    value={star}
+                                    isEdit={editStar}  
+                                    activeColors={["#ffc107"]} 
+                                    style={{justifyContent: 'end'}}
+                                    classNames='mt-2'
+                                />
+                            </Col>
+                        </Row>
                     </Col>
                 </Row>
                 <p className='text-justify'>{review.description}</p>
