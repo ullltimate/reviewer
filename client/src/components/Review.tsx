fbd8f4bfd5a9d87e33f484ec2ac91da5976fbb29
ullltimate <ullltimate.zp@gmail.com>
2023-09-19 12:25:24 +0300

    feat: add download review.pdf

--- client/src/components/Review.tsx
+++ client/src/components/Review.tsx
@@ -15,6 +15,7 @@ import { checkInputs, resetInputs } from '../healpers/healper';
 import CardComment from './CardComment';
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 import Loader from './Loader';
+import { usePDF } from 'react-to-pdf';
 
 function Review() {
     const params = useParams();
@@ -36,6 +37,7 @@ function Review() {
     const [newComment, setNewComment] = useState('');
     const [warning, setWarning] = useState(false);
     const [comments, setComments] = useState<any[]>();
+    const { toPDF, targetRef } = usePDF({filename: 'review.pdf'});
 
     useEffect(()=>{
         if (idReview) getReview(idReview, setReview);
@@ -93,6 +95,10 @@ function Review() {
         }
     }
 
+    function downloadPdf(){
+        (user) ? toPDF() : navigate('/login');
+    }
+
   	return (
   	  <>
 		<Header currentLanguage={currentLanguage} setCurrentLanguage={setCurrentLanguage} changeLanguage={changeLanguage} t={t} theme={theme} setTheme={setTheme}/>
@@ -100,69 +106,78 @@ function Review() {
             review  
             ?
             <Container>
-                <h2 className='my-5 text-center'>{review.nameReview}</h2>
-                <Row className='mb-3'>
-                    <Col md={4} style={{maxHeight: 400}}>
-                        <Image src={review.img} style={{width: '100%', height: '100%', objectFit: 'contain'}} />
-                    </Col>
-                    <Col>
-                        <Row>
-                            <Col>
-                                <h3>{review.title}</h3>
-                            </Col>
-                            <Col className='text-end'>
-                                <span>{rating.toFixed(1)}</span>
-                                <i className="bi bi-star-fill text-warning"></i>
-                                <br />
-                                <small><span className='text-secondary'>{t('cardReview.like')} {amountLikes}</span></small>
-                            </Col>
-                        </Row>
-                        <p>{t('review.group')} {review.group}</p>
-                        <p>{t('review.score')} {review.score}/10</p>
-                        <Row>
-                            <Col>
-                                <p className='mb-0'>{t('review.tags')} #{review.tags.map((e:any) => e).join('#')}</p>
-                            </Col>
-                            <Col className='text-end'>
-                                <ReactStars 
-                                    key={`stars_${star}`}
-                                    onChange={rate} 
-                                    value={star}
-                                    isEdit={editStar}  
-                                    activeColors={["#ffc107"]} 
-                                    style={{justifyContent: 'end'}}
-                                />
-                            </Col>
-                        </Row>
-                        <Row className='align-items-center'>
-                            <Col>
-                                <p className='mb-0'>{t('review.posted')} {Intl.DateTimeFormat(currentLanguage).format(review.creationDate)}</p>
-                            </Col>
-                            <Col className='text-end'>
-                                <Button variant="outline-secondary border-0" onClick={() => like()}>
-                                    <small><i className={`bi bi-heart${(userLike)?'-fill':''}`}></i></small>
-                                </Button>
-                                {
-                                user && 
-                                (JSON.parse(user)._id === review.idAutor) 
-                                ? 
-                                    <>
-                                        <Button variant="outline-secondary border-0" onClick={() => handleShowEdit(review._id)}>
-                                            <small><i className="bi bi-pencil-square"></i></small>
-                                        </Button>
-                                        <Button variant="outline-secondary border-0" onClick={() => {removeReview(review._id); navigate(`/user/${JSON.parse(user)._id}`)}}>
-                                            <small><i className="bi bi-trash3"></i></small>
-                                        </Button>
-                                    </>
-                                : ''
-                                }
-                            </Col>
-                        </Row>
-                    </Col>
-                </Row>
-                <ReactMarkdown children={review.description}></ReactMarkdown>
-                <CreateReview show={showCreate} onHide={() => setShowCreate(false)} update={(edit) ? `${editReview}` : ''}/>
-                <h4>{t('review.comments')} {comments ? comments.length : 0}</h4>
+                <div ref={targetRef}>
+                    <Row className='my-5 align-items-center'>
+                        <Col>
+                            <h2>{review.nameReview}</h2>
+                        </Col>
+                        <Col md={2} className='text-end'>
+                            <Button variant="outline-secondary" onClick={downloadPdf}><i className="bi bi-file-earmark-arrow-down"></i> pdf</Button>
+                        </Col>
+                    </Row>
+                    <Row className='mb-3'>
+                        <Col md={4} style={{maxHeight: 400}}>
+                            <Image src={review.img} style={{width: '100%', height: '100%', objectFit: 'contain'}} />
+                        </Col>
+                        <Col>
+                            <Row>
+                                <Col>
+                                    <h3>{review.title}</h3>
+                                </Col>
+                                <Col className='text-end'>
+                                    <span>{rating.toFixed(1)}</span>
+                                    <i className="bi bi-star-fill text-warning"></i>
+                                    <br />
+                                    <small><span className='text-secondary'>{t('cardReview.like')} {amountLikes}</span></small>
+                                </Col>
+                            </Row>
+                            <p>{t('review.group')} {review.group}</p>
+                            <p>{t('review.score')} {review.score}/10</p>
+                            <Row>
+                                <Col>
+                                    <p className='mb-0'>{t('review.tags')} #{review.tags.map((e:any) => e).join('#')}</p>
+                                </Col>
+                                <Col className='text-end'>
+                                    <ReactStars 
+                                        key={`stars_${star}`}
+                                        onChange={rate} 
+                                        value={star}
+                                        isEdit={editStar}  
+                                        activeColors={["#ffc107"]} 
+                                        style={{justifyContent: 'end'}}
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className='align-items-center'>
+                                <Col>
+                                    <p className='mb-0'>{t('review.posted')} {Intl.DateTimeFormat(currentLanguage).format(review.creationDate)}</p>
+                                </Col>
+                                <Col className='text-end'>
+                                    <Button variant="outline-secondary border-0" onClick={() => like()}>
+                                        <small><i className={`bi bi-heart${(userLike)?'-fill':''}`}></i></small>
+                                    </Button>
+                                    {
+                                    user && 
+                                    (JSON.parse(user)._id === review.idAutor) 
+                                    ? 
+                                        <>
+                                            <Button variant="outline-secondary border-0" onClick={() => handleShowEdit(review._id)}>
+                                                <small><i className="bi bi-pencil-square"></i></small>
+                                            </Button>
+                                            <Button variant="outline-secondary border-0" onClick={() => {removeReview(review._id); navigate(`/user/${JSON.parse(user)._id}`)}}>
+                                                <small><i className="bi bi-trash3"></i></small>
+                                            </Button>
+                                        </>
+                                    : ''
+                                    }
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                    <ReactMarkdown children={review.description}></ReactMarkdown>
+                    <CreateReview show={showCreate} onHide={() => setShowCreate(false)} update={(edit) ? `${editReview}` : ''}/>
+                    <h4>{t('review.comments')} {comments ? comments.length : 0}</h4>
+                </div>
                 {
                 comments 
                 ?
